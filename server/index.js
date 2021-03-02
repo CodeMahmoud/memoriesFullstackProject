6ea75d5e7aca47223a9100e9089138482a853830
mahmoud913 <74220922+mahmoud913@users.noreply.github.com>
2021-03-01 22:23:06 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -3,20 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import mongodb from 'mongodb';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 
 const app = express();
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://mahmoud:mahmoud123@crudwars.dihx0.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+app.get('/', (req, res) => {
+    res.send('Hello There!')
+
+});
+//const CONNECTION_URL = mongo url;
 const PORT = process.env.PORT || 5002;
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port:" ${PORT}`)))
     .catch((error) => console.log(error.message));
 
